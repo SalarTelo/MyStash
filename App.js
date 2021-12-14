3698d24c21ac2300813aba61d1106d36da818bf2
salar <salarmail12@gmail.com>
2021-12-14 18:55:38 +0100

    Changed background color

--- App.js
+++ App.js
@@ -8,14 +8,25 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import { SafeAreaView} from "react-native";
+import {StyleSheet, Text, View} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const App: () => Node = () => {
   return (
-    <SafeAreaView>
-
-    </SafeAreaView>
+    <LinearGradient
+      style={Styles.container}
+      colors={['#c780ff', '#471bc5']}
+      start={{x: 1, y: 0}}
+      end={{x: -1, y: 1}}
+    />
   );
 };
+
+let Styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+    height: '100%',
+  },
+});
 
 export default App;
