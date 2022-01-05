7ee345dbfb082e0bc66119e9c30cd3047879ddce
Salar Telo <salarmail12@gmail.com>
2022-01-05 13:27:46 +0100

    Got a simple camera-screen without any buttons for now.

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { SafeAreaView, Text, View } from "react-native";
+import {
+  SafeAreaView,
 
+  View,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
+import {useCamera} from 'react-native-camera-hooks';
 
-{/*IGNORE THIS*/}
+{
+  /*IGNORE THIS*/
+}
 const CameraScreen = () => {
+  const [{cameraRef}, {takePicture}] = useCamera({
+    autoFocus: true,
+    canDetectBarcode: true,
+    type: RNCamera.Constants.Type.back,
+  });
   return (
-    <SafeAreaView>
-      <Text>Test</Text>
-    </SafeAreaView>
+    <View style={{flex: 1}}>
+      <RNCamera
+        ref={cameraRef}
+        captureAudio={false}
+        style={{
+          flex: 1,
+        }}
+        type={'back'}
+      />
+      <SafeAreaView
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: ' 100%',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        }}
+      />
+    </View>
   );
 };
-
 export default CameraScreen;
