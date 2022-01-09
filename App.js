5a5dd62d7926ff098f74dd59e323d83f1593ff8c
Salar Telo <salarmail12@gmail.com>
2022-01-09 22:53:00 +0100

    Minor changes.

--- App.js
+++ App.js
@@ -8,32 +8,20 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import HomeScreen from './screens/HomeScreen';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Text, View} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import CameraScreen from './screens/CameraScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-function Test({navigation}) {
-  return (
-    <View>
-      <Text>Test</Text>
-    </View>
-  );
-}
 
 const App: () => Node = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-
         <Stack.Screen
-          name={'Camera'}
-          component={CameraScreen}
+          name={'Homescreen'}
+          component={HomeScreen}
           options={{headerShown: false}}
         />
       </Stack.Navigator>
